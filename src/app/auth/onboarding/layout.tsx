8f4d12c63363409ebcaa54d7462806cea54dd36f
Brandon Diaz <61636474+brxndxndiaz@users.noreply.github.com>
2024-04-22 18:41:15 -0400

    Commented out params error
    useSearchParams() is not actively being used as of yet for debugging, i will add a handler to handle the errors for future references

--- src/app/auth/onboarding/layout.tsx
+++ src/app/auth/onboarding/layout.tsx
@@ -6,7 +6,7 @@ export default async function OnboardingLayout({
   return (
     <div>
       <div className="grid place-items-center min-h-screen">
-        <div className="max-w-md w-full p-8 py-8 border-border border rounded bg-slate-50 h-full flex justify-center items-center">
+        <div className="max-w-md w-full p-8 py-8 border-border border rounded bg-slate-50 h-full sm:h-auto flex justify-center items-center">
           {children}
         </div>
       </div>
