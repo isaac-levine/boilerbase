6438906e0df25354fbceefe22be5a35c1edd6caa
Isaac Levine <isaacmlevine4@gmail.com>
2024-04-22 20:54:51 -0400

    move loading to middle of the screen

--- src/app/auth/sign-in/page.tsx
+++ src/app/auth/sign-in/page.tsx
@@ -45,54 +45,52 @@ export default function SignIn() {
     }, 1000);
 
     return (
-      <>
-        <div className="flex flex-col w-full justify-center items-center select-none">
-          <Loader2 className="animate-spin" size={18} />
-          <p className="text-muted">{"Redirecting to dashboard..."}</p>
-        </div> 
-      </>
+      <div className="flex flex-col w-full h-screen justify-center items-center select-none">
+        <Loader2 className="animate-spin" size={18} />
+        <p className="text-muted ">{"Redirecting to dashboard..."}</p>
+      </div>
     );
   }
   return (
-      <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px] h-screen select-none">
-        <div className="flex items-center justify-center py-12 bg-gradient-to-b from-slate-50 to-slate-300 h-full px-12">
-          <div className="mx-auto grid w-[350px] gap-6 ">
-            <div className="grid gap-2 text-center">
-              <h1 className="text-3xl font-bold">Login</h1>
-              <p className="text-balance text-muted-foreground">
-                Enter your email below to login to your account
-              </p>
-            </div>
-            <div className="grid gap-4">
-              <form onSubmit={handleSignIn} className="gap-4 flex flex-col">
-                <div className="grid gap-2">
-                  <Label htmlFor="email">Email</Label>
-                  <Input
-                    id="email"
-                    type="email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    placeholder="m@example.com"
-                    required
-                  />
-                </div>
-                <Button type="submit" className="w-full bg-slate-50 shadow-sm">
-                  Login
-                </Button>
-              </form>
-              <Button
-                variant="outline"
-                className="w-full bg-slate-50 shadow-sm"
-                disabled={true}
-              >
-                <FcGoogle /> Login with Google
+    <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px] h-screen select-none">
+      <div className="flex items-center justify-center py-12 bg-gradient-to-b from-slate-50 to-slate-300 h-full px-12">
+        <div className="mx-auto grid w-[350px] gap-6 ">
+          <div className="grid gap-2 text-center">
+            <h1 className="text-3xl font-bold">Login</h1>
+            <p className="text-balance text-muted-foreground">
+              Enter your email below to login to your account
+            </p>
+          </div>
+          <div className="grid gap-4">
+            <form onSubmit={handleSignIn} className="gap-4 flex flex-col">
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  placeholder="m@example.com"
+                  required
+                />
+              </div>
+              <Button type="submit" className="w-full bg-slate-50 shadow-sm">
+                Login
               </Button>
-            </div>
+            </form>
+            <Button
+              variant="outline"
+              className="w-full bg-slate-50 shadow-sm"
+              disabled={true}
+            >
+              <FcGoogle /> Login with Google
+            </Button>
           </div>
         </div>
-        <div className="hidden bg-muted sm:flex overflow-hidden select-none user-select-none  justify-center items-center w-full h-full">
-          <BoilerBackground />
-        </div>
       </div>
+      <div className="hidden bg-muted sm:flex overflow-hidden select-none user-select-none  justify-center items-center w-full h-full">
+        <BoilerBackground />
+      </div>
+    </div>
   );
 }
