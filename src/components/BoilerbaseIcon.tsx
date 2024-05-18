13d6909244fb067ae2e59c7fbbe8037e2c05608c
Isaac Levine <isaacmlevine4@gmail.com>
2024-05-18 11:48:44 -0400

    icon component

---
+++ src/components/BoilerbaseIcon.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+const BoilerbaseIcon = () => {
+  return (
+    <div>
+      <Image
+        src="/boilerbase-icon.png"
+        alt="Boilerbase Icon"
+        width={30}
+        height={30}
+      />
+    </div>
+  );
+};
+
+export default BoilerbaseIcon;
