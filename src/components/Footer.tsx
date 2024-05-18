db0d504be46445a5e5de2b8e99361eeb88abbb14
Isaac Levine <isaacmlevine4@gmail.com>
2024-05-18 11:59:32 -0400

    hide footer on small screens

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,26 +1,32 @@
-import BoilerbaseIcon from "./BoilerbaseIcon";
 import Link from "next/link";
+import BoilerbaseIcon from "./BoilerbaseIcon";
 
 export const Footer = () => {
   return (
-    <footer className="bg-slate-950 p-2 md:py-10 w-full text-white ">
-      <div className="container max-w-7xl flex flex-col items-center justify-between gap-6 md:flex-row">
-        <Link className="flex items-center gap-2" href="#">
-          <BoilerbaseIcon />
-          <span className="text-lg font-semibold">Boilerbase</span>
-        </Link>
-        <nav className="flex flex-wrap items-center justify-center gap-4 text-sm">
-          <Link className="hover:underline" href="/">
-            Home
+    <footer className="bg-slate-950 p-2 md:py-10 w-full text-white hidden md:flex">
+      <div className="container max-w-7xl flex justify-between gap-2 md:gap-6 flex-row items-start">
+        <div className="flex items-center gap-2 mb-0">
+          <Link href="#" passHref>
+            <div className="flex items-center gap-2 cursor-pointer">
+              <BoilerbaseIcon />
+              <span className="text-lg font-semibold">Boilerbase</span>
+            </div>
           </Link>
-          <Link className="hover:underline" href="#">
-            About
+        </div>
+        <nav className="flex flex-row items-center justify-center gap-4 text-xs md:text-sm">
+          <Link href="/" passHref>
+            <span className="hover:underline cursor-pointer">Home</span>
           </Link>
-          <Link className="hover:underline" href="#">
-            Contact
+          <Link href="#" passHref>
+            <span className="hover:underline cursor-pointer">About</span>
           </Link>
-          <Link className="hover:underline" href="#">
-            Privacy Policy
+          <Link href="#" passHref>
+            <span className="hover:underline cursor-pointer">Contact</span>
+          </Link>
+          <Link href="#" passHref>
+            <span className="hover:underline cursor-pointer">
+              Privacy Policy
+            </span>
           </Link>
         </nav>
         {/* <p className="text-sm text-gray-500 dark:text-gray-400">
