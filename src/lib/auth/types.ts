c8a07699eeb989ec21a20f55e92709c750f66d09
Isaac Levine <isaacmlevine4@gmail.com>
2024-05-22 12:57:46 -0400

    clean up more role stuff

--- src/lib/auth/types.ts
+++ src/lib/auth/types.ts
@@ -1,16 +1,13 @@
-import { Role } from "@prisma/client"
-import { DefaultSession } from "next-auth"
+import { DefaultSession } from "next-auth";
 
 declare module "next-auth" {
-    interface Session {
-        user: {
-            id: string,
-            first_name: string,
-            last_name: string,
-            phone_number: string,
-            onboarded: boolean,
-            role: Role,
-            
-        } & DefaultSession["user"]
-    }
+  interface Session {
+    user: {
+      id: string;
+      first_name: string;
+      last_name: string;
+      phone_number: string;
+      onboarded: boolean;
+    } & DefaultSession["user"];
+  }
 }
