110379036f63af52757be23df3c1f663dd9b0097
Isaac Levine <isaacmlevine4@gmail.com>
2024-06-13 22:22:07 -0400

    blocks icon

---
+++ src/components/BoilerbaseIconBlocks.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+
+interface BoilerbaseIconBlocksProps {
+  size: number;
+}
+
+const BoilerbaseIconBlocks: React.FC<BoilerbaseIconBlocksProps> = ({
+  size,
+}) => {
+  return (
+    <div>
+      <Image
+        src="/boilerbase-icon-transparent.png"
+        alt="Boilerbase Icon"
+        width={size}
+        height={size}
+        className="invert dark:invert-0"
+      />
+    </div>
+  );
+};
+
+export default BoilerbaseIconBlocks;
