9d1fe47a879209c5c1965434775a9935a4badfb6
Isaac Levine <isaacmlevine4@gmail.com>
2024-06-14 00:06:21 -0400

    spacing

--- src/app/new-landing/HeroSection.tsx
+++ src/app/new-landing/HeroSection.tsx
@@ -2,15 +2,16 @@ import React from "react";
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import { Button } from "@/components/ui/button";
 import BoilerbaseIconBlocks from "@/components/BoilerbaseIconBlocks";
+import { ChevronRight } from "lucide-react";
 
 const HeroSection = () => {
   return (
     <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48">
       <MaxWidthWrapper>
-        <div className="grid gap-6 lg:grid-cols-[2fr_1fr] xl:grid-cols-[2fr_1fr]">
+        <div className="grid gap-6 lg:grid-cols-[3fr_2fr]">
           <div className="flex flex-col justify-center space-y-4 text-center lg:text-left">
             <div className="space-y-2">
-              <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl">
+              <h1 className="text-3xl font-bold sm:text-5xl xl:text-6xl">
                 Ship your SaaS app 90% faster with{" "}
                 <span className="text-primary font-extrabold">Boilerbase</span>.
               </h1>
@@ -20,8 +21,10 @@ const HeroSection = () => {
               </p>
             </div>
             <div className="flex flex-col gap-2 min-[400px]:flex-row mx-auto lg:mx-0">
-              <Button variant="default" size="lg">
-                Get Started
+              <Button variant="default" className="flex flex-row gap-2">
+                Get Started {"  "}
+                <BoilerbaseIconBlocks size={24} />
+                {/* <ChevronRight size={24} /> */}
               </Button>
             </div>
           </div>
