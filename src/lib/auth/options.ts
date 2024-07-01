ff950bce8abc6544aee97990ad2212268c19b537
Isaac Levine <isaacmlevine4@gmail.com>
2024-06-30 22:31:06 -0400

    add next auth secret

--- src/lib/auth/options.ts
+++ src/lib/auth/options.ts
@@ -92,7 +92,7 @@ export const authOptions = {
       clientSecret: `${process.env.GITHUB_SECRET}`,
     }),
   ],
-
+  secret: process.env.NEXTAUTH_SECRET,
   adapter: PrismaAdapter(prisma),
 } as AuthOptions;
 
