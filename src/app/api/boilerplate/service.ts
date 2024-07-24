01b1c87f921411d3027893c2728732f82fc654e1
Isaac Levine <isaacmlevine4@gmail.com>
2024-07-23 22:51:19 -0400

    Refactor: service functions into separate file

---
+++ src/app/api/boilerplate/service.ts
@@ -0,0 +1,37 @@
+import { Resend } from "resend";
+import {
+  GenerationEmail,
+  MagicLinkEmail,
+} from "../../../lib/email/templates/magic-link";
+
+export const resend = new Resend(process.env.RESEND_SECRET);
+export const resendDomain = process.env.RESEND_DOMAIN;
+
+export async function sendMagicLinkEmail(to: string, signInLink: string) {
+  const data = await resend.emails.send({
+    from: `Boilerbase <help@${resendDomain}>`,
+    to: [to],
+    subject: "Magic sign-in link",
+    react: MagicLinkEmail({ signInLink: signInLink, sentTo: to }),
+  });
+}
+
+export const sendGenerationEmailToIsaac = async (
+  githubUsername: string,
+  cliCommand: string
+) => {
+  if (!githubUsername || githubUsername === "") {
+    console.error(
+      "No github username provided. Can not send generation email to Isaac"
+    );
+  }
+  const data = await resend.emails.send({
+    from: `Boilerbase <help@${resendDomain}>`,
+    to: "isaacmlevine4@gmail.com",
+    subject: `Customer ${githubUsername} requested a Boilerplate!`,
+    react: GenerationEmail({
+      githubUsername: githubUsername,
+      cliCommand: cliCommand,
+    }),
+  });
+};
