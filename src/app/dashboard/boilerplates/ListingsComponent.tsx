42fb94294695c202b00da1a179281329939589b9
Isaac Levine <isaacmlevine4@gmail.com>
2025-01-01 20:46:35 -0500

    make listing search more responsive

--- src/app/dashboard/boilerplates/ListingsComponent.tsx
+++ src/app/dashboard/boilerplates/ListingsComponent.tsx
@@ -53,16 +53,16 @@ export default function ListingsComponent() {
 
   return (
     <div className="my-12">
-      <div className="flex items-center justify-between mb-8">
-        <h1 className="text-2xl font-bold text-[--background] sm:block hidden dark:text-[--foreground]">
+      <div className="flex flex-col lg:flex-row items-center justify-between mb-8 gap-4">
+        <h1 className="text-2xl font-bold text-[--background] lg:block hidden dark:text-[--foreground]">
           Discover pre-built boilerplates from the community.
         </h1>
-        <div className="flex items-center space-x-4">
-          <div className="relative w-full max-w-md">
+        <div className="flex items-center w-full lg:w-auto">
+          <div className="relative w-full lg:max-w-md">
             <SearchIcon className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500 dark:text-gray-400" />
-            <form onSubmit={handleSearch}>
+            <form onSubmit={handleSearch} className="w-full">
               <Input
-                className="pl-10 pr-4 py-2 rounded-md bg-gray-100 dark:bg-gray-800 dark:text-gray-200 focus:outline-none focus:ring-2 focus:ring-primary focus:border-transparent"
+                className="pl-10 pr-4 py-2 rounded-md bg-gray-100 dark:bg-gray-800 dark:text-gray-200 focus:outline-none focus:ring-2 focus:ring-primary focus:border-transparent w-full"
                 placeholder="Search boilerplates..."
                 onChange={(e) => setQuery(e.target.value)}
                 type="text"
@@ -76,7 +76,7 @@ export default function ListingsComponent() {
           <PulseLoader loading={loading} size={10} color="#2563EB" />
         </div>
       ) : (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+        <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-6">
           {listings.map((listing) => (
             <BoilerplateCard key={listing.id} listing={listing} />
           ))}
